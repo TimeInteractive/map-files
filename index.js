db2e64caa605b942c5f7849c29ca84870974e551
Chris Wilson <christopher.e.wilson@gmail.com>
2015-07-07 12:14:21 -0400

    Have make script calling Node correctly

---
+++ index.js
@@ -0,0 +1,59 @@
+var fs = require("fs");
+var args = require('minimist')(process.argv.slice(2));
+var streamCSV = require("streamCSV");
+
+var properties = require("./census/properties.json");
+var fips = require("./census/fips.json");
+var index = {};
+
+
+// load the raw census data and filter down to the properties we've identified we might want in properties.json
+streamCSV({
+		filename: "census/ACS_13_5YR_DP05/ACS_13_5YR_DP05_with_ann.csv",
+		dontguess: "GEO.id,GEO.id2"
+	},
+    function(line, c) {
+    	var location = {};
+    	for (var prop in line) {
+    		if (properties[prop]) {
+    			location[properties[prop]] = line[prop];
+    		}
+    	}
+    	index[location.fips] = location;
+    },
+    function() { // on close 
+    	try {
+	    	var geography = require("./topojson/" + args._[0]);
+	    } catch (e) {
+	    	console.log(e);
+	    	return;
+	    }
+	    for (var geo_type in geography.objects) {
+	    	var geometries = geography.objects[geo_type].geometries;
+	    	geometries.forEach(function(geometry) {
+	    		if (!geometry.properties) {
+	    			geometry.properties = {};
+	    		}
+	    		// they all have a FIPS code, so let's add state name and abbr
+	    		geometry.properties.state = fips[geometry.properties.st][0];
+	    		geometry.properties.abbr = fips[geometry.properties.st][1];
+
+	    		//console.log(geometry);
+    			geometry.properties.fips = index[geometry.id].fips;
+    			if (args.properties) {
+    				args.properties.split(",").forEach(function(prop) {
+    					prop = prop.toLowerCase();
+    					if (index[geometry.id].hasOwnProperty(prop)) {
+    						geometry.properties[prop] = index[geometry.id][prop];
+    					} else {
+    						console.log("Couldn't find a value for '" + prop + "' for " + geometry.properties.fips);
+    					}
+    				});
+    			}
+	    	});
+	    }
+	    fs.writeFileSync("./topojson/rich_" + args._[0], JSON.stringify(geography));
+
+
+    }       
+);
--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 path=""
 PS3="Please choose a map type:"
 select option in states counties congressional
@@ -30,6 +31,10 @@ unzip "$filename";
 
 basename=${filename%.zip}
 
+
+#echo "What sort of quantization do you want? Default is 1e5. You can just enter '6' for 1e6 if you want."
+#read quantization
+
 select projection in "pre-projected" "not pre-projected"
 do
     case $projection in
@@ -58,5 +63,56 @@ do
 	esac
 done
 
+if [[ "$projection" == 'pre-projected' ]]; then
+	topo_file="$option.preprojected.topo.json"
+else 
+	topo_file="$option.topo.json"
+fi	
+
+echo "$topo_file"
+
 cd ..
-#rm -rf shp
+rm -rf shp
+
+
+#now let's add some properties from the Census tables. First we'll load them from the JSON file
+
+PROPERTIES=()
+
+re=": \"(.*)\""
+while IFS='' read -r line || [[ -n $line ]]; do
+	if [[ $line =~ $re ]]; then
+        name="${BASH_REMATCH[1]}"
+        PROPERTIES+=($name);
+    fi
+done < "census/properties.json"
+
+menu() {
+    echo "Avaliable options:"
+    for i in ${!PROPERTIES[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${PROPERTIES[i]}"
+    done
+    [[ "$msg" ]] && echo "$msg"; :
+}
+
+prompt="Check an option (again to uncheck, ENTER when done): "
+while menu && read -rp "$prompt" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#PROPERTIES[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); msg="${PROPERTIES[num]} was ${choices[num]:+un}checked"
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+done
+
+myprops=""
+msg=""
+for i in ${!PROPERTIES[@]}; do 
+    [[ "${choices[i]}" ]] && { myprops+="${PROPERTIES[i]},"; }
+done
+
+#remove trailing comma
+myprops="${myprops%?}";
+
+echo "You chose $myprops";
+
+node index.js "$topo_file" --properties="$myprops"
